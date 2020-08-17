7f73f39a02dbcbd7aea00c617207400e754d4b40
McLavish <Luca.renna.studenti@isii.it>
2020-08-18 01:31:22 +0200

    Fixed bug: team mobs wouldn't attack other players when a team member was nearby Added feature: now mobs on different teams will fight each other (only when a player is nearby)

--- src/main/java/mactavish/firstplugin/events/EntityTarget.java
+++ src/main/java/mactavish/firstplugin/events/EntityTarget.java
@@ -3,10 +3,13 @@ package mactavish.firstplugin.events;
 import mactavish.firstplugin.utils.Team;
 import mactavish.firstplugin.utils.TeamsManager;
 import org.bukkit.entity.Creature;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetEvent;
+
+import java.util.List;
 
 public class EntityTarget implements Listener {
 
@@ -20,7 +23,34 @@ public class EntityTarget implements Listener {
       Creature mob = (Creature) event.getEntity();
 
       Team playerTeam = teamsManager.getTeam(player);
-      if (playerTeam != null && playerTeam == teamsManager.getTeam(mob)) {
+      Team mobTeam = teamsManager.getTeam(mob);
+
+      if (playerTeam != null && playerTeam == mobTeam) {
+
+        List<Entity> nearbyEntities = mob.getNearbyEntities(10,5,10);
+
+        //Codice schifo fatto all'una di notte, cambiare pls
+        for (Entity entity : nearbyEntities) {
+
+          if (entity instanceof Creature) {
+            Creature targetMob = (Creature) entity;
+            Team targetTeam = teamsManager.getTeam(targetMob);
+
+            if (targetTeam == null || targetTeam != mobTeam) {
+              mob.setTarget(targetMob);
+              break;
+            }
+
+          } else if (entity instanceof Player) {
+            Player targetPlayer = (Player) entity;
+            Team targetTeam = teamsManager.getTeam(targetPlayer);
+
+            if (targetTeam == null || targetTeam != mobTeam) {
+              mob.setTarget(targetPlayer);
+              break;
+            }
+          }
+        }
         event.setCancelled(true);
       }
     }
